fcc609206b6cfe545f71f15a53ed3510be0adc76
rodriguembiabo <rodriguembiabo@gmail.com>
2022-09-05 13:55:20 +0000

    update

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,18 +1,13 @@
 #!/usr/bin/node
 // Computes and prints a factorial recursively
 
-function factorial (n) {
-  if (n === 1) {
-    return (1);
-  }
-  return (n * factorial(n - 1));
+const num = parseInt(process.argv[2]);
+
+function factorial(x) {
+	if (x === 0){
+		return 1;
+	}
+	return x * factorial(x-1);
 }
 
-const args = process.argv;
-
-if (isNaN(args[2])) {
-  console.log('1');
-} else {
-  let num = factorial(parseInt(args[2], 10));
-  console.log(num);
-}
+console.log(factorial(num));
