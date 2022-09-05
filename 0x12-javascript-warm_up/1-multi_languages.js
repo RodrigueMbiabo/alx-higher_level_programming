6676e118628b13721582d226d0f7e5c1fd96a0be
rodriguembiabo <rodriguembiabo@gmail.com>
2022-09-05 13:39:52 +0000

    update

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -3,7 +3,7 @@
 
 const firstLine = 'C is fun';
 const secondLine = 'Python is cool';
-const thirdLine = 'Javascript is amazing';
+const thirdLine = 'JavaScript is amazing';
 
 console.log(firstLine + '\n' + secondLine + '\n' +
                 thirdLine);
