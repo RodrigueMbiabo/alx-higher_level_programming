4cfa0d926dedb1a73784cf2a76f66ae0f70b8cf5
RodrigueMbiabo <rodriguembiabo@gmail.com>
2022-09-22 15:52:02 -0700

    other version

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Script that makes a request to causes an specific response
-curl -sL 0.0.0.0:5000/catch_me_3 -X PUT -H "Origin:School"
+# display methods
+curl -s -X POST $1 -d @"$2" --header "Content-Type: application/json"
