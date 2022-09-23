4b75ca36183f7f8777bcfcf3166b26d786cbe21a
RodrigueMbiabo <rodriguembiabo@gmail.com>
2022-09-23 13:31:46 -0700

    missing error on task 5

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-""" Python script that fetches https://intranet.hbtn.io/status """
-
-
+"""
+fetches https://intranet.hbtn.io/status
+"""
 if __name__ == "__main__":
-    from urllib import request
-    with request.urlopen('https://intranet.hbtn.io/status') as response:
-        html = response.read()
-        print("Body response:")
+    import urllib.request as request
+    with request.urlopen('https://intranet.hbtn.io/status') as r:
+        html = r.read()
+        print('Body response:')
         print("\t- type: {}".format(type(html)))
         print("\t- content: {}".format(html))
-        print("\t- utf8 content: {}".format(html.decode('utf8')))
+        print("\t- utf8 content: {}".format(html.decode('utf-8')))
