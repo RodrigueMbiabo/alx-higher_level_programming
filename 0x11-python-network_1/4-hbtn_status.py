bb37c8626e556d33dee31ffa0c751a93b844c77a
RodrigueMbiabo <rodriguembiabo@gmail.com>
2022-09-23 13:46:37 -0700

    taks 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-"""
-fetches https://intranet.hbtn.io/status
-"""
-if __name__ == '__main__':
-    import requests
+"""Fetches https://intranet.hbtn.io/status"""
+import requests
+
+if __name__ == "__main__":
+
     r = requests.get('https://intranet.hbtn.io/status')
-    text = r.text
-    print("Body response:")
-    print("\t- type: {}".format(type(text)))
-    print("\t- content: {}".format(text))
+
+    print('Body response:')
+    print('\t- type: {}'.format(type(r.text)))
+    print('\t- content: {}'.format(r.text))
